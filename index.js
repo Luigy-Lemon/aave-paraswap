1c3a68b37c72a2f9e8546a23bbf6c48a7ee00203
luigy-lemon <luigy-lemon@protonmail.com>
2024-10-24 17:25:02 +0100

    feat: subgraph upgrade and included Base

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+import dotenv from 'dotenv';
 import fetch from "node-fetch";
 import fs from "fs";
 import { ethers, FixedNumber } from "ethers";
 
-const GRAPHQL_URL = "https://api.thegraph.com/subgraphs/name/paraswap/paraswap-subgraph";
+dotenv.config();
+const GRAPHQL_KEY = process.env.GRAPHQL_KEY;
 
-const chains = ["bsc"]//["eth", "polygon", "arbitrum", "optimism", "avalanche", "bsc"];
-const gecko_chains = ["bsc"]//["eth", "polygon_pos", "arbitrum", "optimism", "avax", "bsc"];
+//const GRAPHQL_KEY = "a88dbeb57fb8f9d8ddc9a8f973ccb8f1"; //Required
+const GRAPHQL_URL = `https://gateway.thegraph.com/api/${GRAPHQL_KEY}/subgraphs/id/`;
+
+const chains = ["eth", "polygon", "arbitrum", "optimism", "avalanche", "bsc","base"];
+const chain_ids = ["8k74P7fPtsB5EZu53iDGQUMHtWuAH4YCKwBawySvkhSa", "D72KzovXDszkzbkaekhAGY3j3nA2GHbusuikk7QsDX8G", "AKxQrWRiaAq2cH1hBeVTqo95gnG6NrjVeLXXp4b1DVdG", "CxWHhhC2gaaFSgVPqACipZQMQesWUxWc1fmESNCjNkf8", "DMJXB2sBBXD66Lyk3dBEpktQwHX9Vu2hirDCKmLgPWQ8", "2aWZK7r2mhBjwxs5yEsuJVEnhmnoppHm7RufzqQKLqQf","9y2jXBMqwgnt9TQpd9Wnj9DuYyaxnM2nLVAqZQ8SPyJq"]
+const gecko_chains = ["eth", "polygon_pos", "arbitrum", "optimism", "avax", "bsc","base"];
 
 function coingeckoUrl(chainIndex, token) {
   let chain = gecko_chains[chainIndex];
@@ -20,20 +26,29 @@ async function queryPartnerFees(chain) {
   // Construct a schema, using GraphQL schema language
   const querySchema = `
     {
-      partnerFees(first: 100, where: {partnerAddress: "0x9abf798f5314bfd793a9e57a654bed35af4a1d60"}, orderBy:"totalRewards", orderDirection: desc) {
+      partnerFees(first: 100, where: {partnerAddress: "0x9abf798f5314bfd793a9e57a654bed35af4a1d60"}, orderBy:totalRewards, orderDirection: desc) {
         tokenAddress
         totalRewards
       }
     }
   `;
-  let url = (chain === "eth") ? GRAPHQL_URL : GRAPHQL_URL + "-" + chain;
+    const querySchemaBase = `
+    {
+      partnerFees(first: 100, where: {partnerAddress: "0xAe940e61E9863178b71500c9B5faE2a04Da361a1"}, orderBy:totalRewards, orderDirection: desc) {
+        tokenAddress
+        totalRewards
+      }
+    }
+  `;
+
+  let url =  GRAPHQL_URL + chain_ids[chains.indexOf(chain)];
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "content-type": "application/json",
     },
     body: JSON.stringify({
-      query: querySchema,
+      query: (chain === "base") ? querySchemaBase : querySchema,
     }),
   });
 
@@ -61,6 +76,7 @@ async function fetchAllChains() {
     const newFees = await queryPartnerFees(chains[i]);
     chainFees[i] = newFees;
     console.log(`${chains[i]}: ${newFees.length} tokens have unclaimed fees`);
+    console.log(chainFees[i].map(a => a.tokenAddress))
   }
   return chainFees;
 }
@@ -78,7 +94,6 @@ async function data() {
       let paraswap_fees_USD = (chainFees[i][j].totalRewards && tokenInfo["price_usd"] && tokenInfo["decimals"]) ? FixedNumber.fromValue(chainFees[i][j].totalRewards, tokenInfo["decimals"]).mul(FixedNumber.fromString(tokenInfo["price_usd"])).toString() : "0";
 
       let tokenData = {};
-      tokenData["chain"] = chains[i];
       tokenData = {
         chain: chains[i],
         ...tokenInfo,
